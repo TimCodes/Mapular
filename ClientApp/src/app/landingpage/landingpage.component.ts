3d183fa8cf5a197567d11b597482622fef7733cb
timcodes <hardtimo@isu.edu>
2020-07-14 18:16:24 -0600

    worked on landing page

--- ClientApp/src/app/landingpage/landingpage.component.ts
+++ ClientApp/src/app/landingpage/landingpage.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landingpage',
   templateUrl: './landingpage.component.html',
-  styleUrls: ['./landingpage.component.css']
+  styleUrls: ['./landingpage.component.scss'],
 })
 export class LandingpageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
